b3371c8b1ba571ccaa3dd965896922619e710ce1
chaowen guo <chaowen.guo1@gmail.com>
2021-09-27 06:53:23 -0500

    Update build.py

--- build.py
+++ build.py
@@ -55,7 +55,7 @@ async def mysql(session, token):
 async def main():
     async with aiohttp.ClientSession() as session:
         async with session.post(f'https://login.microsoftonline.com/{args.tenantid}/oauth2/token', data={'grant_type':'client_credentials', 'client_id':args.clientid, 'client_secret':args.clientsecret, 'resource':'https://management.azure.com/'}) as response:
-            token = (await response.json).get('access-token')
+            token = (await response.json()).get('access_token')
             await asyncio.gather(postgres(session, token), mysql(session, token))
             
 asyncio.run(main())

5e5b142f40c602aa474c309ecfc02855d887315d
chaowen guo <chaowen.guo1@gmail.com>
2021-09-27 02:43:53 -0500

    Update build.py

--- build.py
+++ build.py
@@ -9,7 +9,7 @@ args = parser.parse_args()
 async def main():
     async with aiohttp.ClientSession() as session:
         async with session.post(f'https://login.microsoftonline.com/{args.tenantid}/oauth2/token', data={'grant_type':'client_credentials', 'client_id':args.clientid, 'client_secret':args.clientsecret, 'resource':'https://management.azure.com/'}) as response:
-            print(await response.json())
+            print((await response.json()).access_token)
 
 asyncio.run(main())
 

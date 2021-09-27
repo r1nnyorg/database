4259a02c483617a272845035acbba091a793a426
chaowen guo <chaowen.guo1@gmail.com>
2021-09-27 01:45:35 -0500

    Update build.py

--- build.py
+++ build.py
@@ -1,1 +1,9 @@
-import aiohttp
+import aiohttp, asyncio
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://httpbin.org/get') as resp:
+            print(resp.status)
+            print(await resp.text())
+
+asyncio.run(main())

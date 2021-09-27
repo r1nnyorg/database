773ea8856c2f62ea7c7d26c71f832042803ed517
chaowen guo <chaowen.guo1@gmail.com>
2021-09-27 05:07:41 -0500

    Update build.py

--- build.py
+++ build.py
@@ -27,6 +27,7 @@ async def main():
                         await asyncio.sleep(int(response.headers.get('retry-after')))
                         async with session.get(response.headers.get('azure-asyncOperation'), headers={'Authorization':f'Bearer {token}'}) as _:
                             print((await _.json()).get('status'))
+                            if (await _.json()).get('status') == 'Succeeded': break
             async with session.put(f'https://management.azure.com/subscriptions/{subscription}/resourceGroups/postgres/providers/Microsoft.DBForPostgreSql/flexibleServers/postgrespostgres/firewallRules/postgres?api-version=2020-02-14-preview', headers={'Authorization':f'Bearer {token}'}, json={'properties':{'startIpAddress':'0.0.0.0','endIpAddress':'255.255.255.255'}}) as response:
                 print(response.status)
                 

f5bb5a6f3897f21daa657541d4928ff175181591
chaowen guo <chaowen.guo1@gmail.com>
2021-10-27 05:39:20 -0500

    Update build.py

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-import aiohttp, asyncio, argparse, asyncpg, pathlib, asyncmy, ssl, oci
+import aiohttp, asyncio, argparse, asyncpg, pathlib, asyncmy, ssl, oci, cx_Oracle
 
 configure = {'user':'ocid1.user.oc1..aaaaaaaalwudh6ys7562qtyfhxl4oji25zn6aapndqfuy2jfroyyielpu3pa', 'key_file':'oci.key', 'fingerprint':'bd:01:98:0d:5d:4a:6f:b2:49:b4:7f:df:43:00:32:39', 'tenancy':'ocid1.tenancy.oc1..aaaaaaaa4h5yoefhbxm4ybqy6gxl6y5cgxmdijira7ywuge3q4cbdaqnyawq', 'region':'us-sanjose-1'}
 databaseClient = oci.database.DatabaseClient(configure)
